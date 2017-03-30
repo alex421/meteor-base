fb90cc13a0afff3d89f719fd31ff5bd15bd2bd4e
alex421 <alex421@gmail.com>
2017-03-30 09:50:53 +0300

    userslist commit

--- imports/ui/containers/UsersList.js
+++ imports/ui/containers/UsersList.js
@@ -8,8 +8,26 @@ import Loading from '../components/Loading.js';
 const composer = (params, onData) => {
   const subscription = Meteor.subscribe('users.list');
   if (subscription.ready()) {
-    const users = Meteor.users.find({companyID: params.companyID}).fetch();
-    //console.log("users",users)
+    //const users = Meteor.users.find({companyID: params.companyID}).fetch();
+    const result= Meteor.users.find({"information.companyID": params.companyID}).fetch()
+
+
+  var users=result.map((user)=>{
+   return {
+            "name":user.profile.name.first,
+            "surname":user.profile.name.last,
+            "tel":user.information.tel,
+            "fonction":user.information.fonction,
+            "dateCreated":user.information.dateCreated,
+            "email":user.emails[0].address,
+            "password":"xxxxx",
+            "companyID":user.information.companyID,
+            "_id":user._id
+          }
+  })
+
+    console.log("ALLusers",Meteor.users.find().fetch())
+    console.log("Some users",Meteor.users.find({"information.companyID": params.companyID}).fetch())
     onData(null, { users });
   }
 };
